190a717ee9036d24a2e7465bb2ea0a9d45913cb0
bo <347232897@qq.com>
2021-06-29 08:03:41 +0800

    增加一个检查URL有效性的函数

--- myfun.sh
+++ myfun.sh
@@ -7,3 +7,15 @@ function isdigit() {
       echo 0
     fi
 }
+
+# 判断URL是否有效
+
+function check_url() {
+    wget --spider -q -o /dev/null --tries=1 -T 5 $1 
+    if [ $? -eq 0 ]      
+    then        
+	echo "$1 is yes."    
+    else        
+	echo "$1 is no."    
+    fi
+}
